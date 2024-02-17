d7a3037852d9fb41f130bdec2a38b8b86b0f3c1b
Ivan Murashko <ivan.murashko@gmail.com>
2024-02-17 11:37:44 +0000

    Chapter 3 source code updates

--- chapter3/declvisitor/build.sh
+++ chapter3/declvisitor/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ./cleanup.sh
-export LLVM_HOME=`pwd`/../../../../llvm-project-16.x/install
+export LLVM_HOME=`pwd`/../../../llvm-project/install
 mkdir build
 cd build
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug ..
